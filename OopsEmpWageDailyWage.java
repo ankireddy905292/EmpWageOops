a044d7d3e1ecb04db97c30406e198fd8e43142c9
ankireddy905292 <91325607+ankireddy905292@users.noreply.github.com>
2021-10-24 15:49:50 +0530

    Add files via upload

---
+++ OopsEmpWageDailyWage.java
@@ -0,0 +1,36 @@
+package com.bridgelabz;
+import java.util.Random;
+
+public class EmpWageOops {
+
+    public static void main(String[] args) {
+
+        Attendance attendance = new Attendance();
+        attendance.EmployeeCheck();
+    }
+}
+    class Attendance{
+     static final int IS_PRESENT = 1;
+     static final int WAGE_PER_HOUR = 20;
+        static final int IS_FULL_TIME = 8;
+        int dailyWage = 0;
+
+    int GetEmployeePresent(int a) {
+        Random r = new Random();
+        int empCheck = r.nextInt(a);
+        return empCheck;
+    }
+    void EmployeeCheck() {
+        int empCheck = GetEmployeePresent(2);
+        if (empCheck == IS_PRESENT) {
+            System.out.println("Employee is present");
+            dailyWage = WAGE_PER_HOUR * IS_FULL_TIME;
+
+        }
+        else {
+            System.out.println("Employee is Absent");
+            dailyWage = dailyWage;
+        }
+        System.out.println("Daily Wage : " + dailyWage);
+    }
+}
---
+++ OopsEmpWageSwitchCase.java
@@ -0,0 +1,47 @@
+package com.bridgelabz;
+import java.util.Random;
+
+    public class EmpWageOops {
+
+        public static void main(String[] args) {
+            Attendance attendance = new Attendance();
+            attendance.EmployeeCheck();
+    }
+}
+    class Attendance{
+        static final int IS_PRESENT = 1;
+        static final int IS_ABSENT = 0;
+        static final int WAGE_PER_HOUR = 20;
+        static final int IS_FULL_TIME = 8;
+        static final int IS_PART_TIME = 4;
+        int dailyWage = 0;
+
+    int GetEmployeePresent(int a) {
+        Random r = new Random();
+        int empCheck = r.nextInt(a);
+        return empCheck;
+    }
+
+    void EmployeeCheck() {
+        int empCheck = GetEmployeePresent(3);
+        switch (empCheck) {
+            case IS_PRESENT: {
+                System.out.println("Employee is present");
+                dailyWage = WAGE_PER_HOUR * IS_FULL_TIME;
+                break;
+
+            }
+            case IS_ABSENT: {
+                System.out.println("Employee is Absent");
+                dailyWage = dailyWage;
+                break;
+            }
+            default: {
+                System.out.println("Employee is Part time");
+                dailyWage = WAGE_PER_HOUR * IS_PART_TIME;
+                break;
+            }
+        }
+        System.out.println("Daily Wage : " + dailyWage);
+    }
+}
